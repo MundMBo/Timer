25726dcd46f54382314389e1036242ab9e1c1030
MundMBo <MuellerMarvin@outlook.com>
2020-11-19 15:35:34 +0100

    timer updated
    set duration button added
    set end time added

--- timer.js
+++ timer.js
@@ -2,12 +2,94 @@
 $(document).ready(function() {
     
 	// init timer
-	$("#timer").TimeCircles({
-		"animation": "smooth",				// "smooth", "ticks"
+	$.fn.initTimer();
+	
+	// init buttons start
+	$("#start").click(function() {
+		$(".timer").TimeCircles().start();
+	});
+	
+	// init buttons stop
+	$("#stop").click(function() {
+		$(".timer").TimeCircles().stop();
+	});
+	
+	// init buttons reset
+	$("#reset").click(function() {
+		$(".timer").TimeCircles().restart();
+		$(".timer").TimeCircles().stop();
+	});
+	
+	// init buttons increment
+	$(".btn-decrement").click(function() {
+		var obj = $(this).closest(".input-group").children("input");
+		var input = obj.val();
+		if (input == "") {input = 0;}
+		input = parseInt(input);
+		input = input - 1;
+		obj.val(input);
+	});
+	
+	// init buttons decrement
+	$(".btn-increment").click(function() {
+		var obj = $(this).closest(".input-group").children("input");
+		var input = obj.val();
+		if (input == "") {input = 0;}
+		input = parseInt(input);
+		input = input + 1;
+		obj.val(input);
+	});
+	
+	// init button set duration
+	$("#setDuration").click(function() {
+		$(".timer").TimeCircles().destroy();
+		$(".timer").removeAttr("data-date");
+		$(".timer").removeAttr("data-timer");
+		$(".config").attr("data-timer-new", 0);
+		$(this).closest("form").find("input").each(function () {
+			var current = parseInt($(".config").attr("data-timer-new"));
+			var value = parseInt($(this).val());
+			var factor = parseInt($(this).attr("data-factor-to-seconds"));
+			if (value > 0 && factor > 0) {
+				$(".config").attr("data-timer-new",  current + value * factor);
+			}
+		});
+		$(".timer").attr("data-timer", $(".config").attr("data-timer-new"));
+		$(".config").removeAttr("data-timer-new");
+		$.fn.initTimer();
+	});
+	
+	// init button set end time
+	$("#setEndTime").click(function() {
+		$(".timer").TimeCircles().destroy();
+		$(".timer").removeAttr("data-date");
+		$(".timer").removeAttr("data-timer");
+		$(".config").attr("data-date-new", 0);
+		$(this).closest("form").find("input").each(function () {
+			var current = parseInt($(".config").attr("data-date-new"));
+			var value = parseInt($(this).val());
+			// required output "2014-01-01 00:00:00"
+		});
+		$(".timer").attr("data-date", $(".config").attr("data-date-new"));
+		$(".config").removeAttr("data-date-new");
+		$.fn.initTimer();
+	});
+});
+
+// refresh
+$(window).resize(function() {
+    $(".timer").TimeCircles().rebuild();
+});
+
+// init timer function
+$.fn.initTimer = function(){ 
+	$(".timer").TimeCircles({
+		"animation": "smooth",					// "smooth", "ticks"
+		"count_past_zero": false,
 		"bg_width": 1.2,
 		"fg_width": 0.1,
-		"direction": "Clockwise",				// "Clockwise", "Counter-clockwise", "Both"
 		"circle_bg_color": "#60686F",
+		"direction": "Clockwise",				// "Clockwise", "Counter-clockwise", "Both"
 		"time": {
 			"Days": {
 				"text": "Days",
@@ -30,33 +112,6 @@ $(document).ready(function() {
 				"show": true
 			}
 		},
-		start: false
+		"start": false
 	});
-	
-	// define ending behaviour
-	$("#timer").TimeCircles().end().stop(); 
-	
-	// init buttons start
-	$("#start").click(function() {
-		$("#timer").TimeCircles().start();
-	});
-	
-	// init buttons stop
-	$("#stop").click(function() {
-		$("#timer").TimeCircles().stop();
-	});
-	
-	// init buttons reset
-	$("#reset").click(function() {
-		$("#timer").TimeCircles().restart();
-		$("#timer").TimeCircles().stop();
-	});
-
-});
-
-// refresh
-$(window).resize(function() {
-    $("#timer").TimeCircles().rebuild();
-});
-
-
+}
